72af65881e3c19e651e5c4086c6b77067cfb23f6
Devansh Chauhan <devansh@Devanshs-MacBook-Pro.local>
2026-04-07 18:23:48 +0530

    4th commit

--- main/home/home.js
+++ main/home/home.js
@@ -1,14 +1,13 @@
 async function fetchRecipes(searchQuery) {
-    try {
+    try{
+
         const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchQuery}`);
         const data = await response.json();
         const recipes = data.meals;
         console.log("Here is your fetched data:", recipes);
-    } 
-    
-    catch (error) {
-        console.error("Oops! Something went wrong while fetching:", error);
+    }
+    catch(error){
+        console.error("Not able to fetch data!",error);
     }
 }
-
-fetchRecipes("Paneer");
+fetchRecipes("Potato");
